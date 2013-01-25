a9d28d15e471655742fc7ca375ea55009e186cc6
Alex Bennett <me@alexben.net>
2013-01-24 21:48:38 -0600

    Forgot to change something...

--- src/com/legit2/Demigods/DFlatFile.java
+++ src/com/legit2/Demigods/DFlatFile.java
@@ -107,7 +107,7 @@ public class DFlatFile
 			String load = element.getName();
 			if (load.endsWith(".demi"))
 			{
-				load = load.substring(0, load.length() - 7);
+				load = load.substring(0, load.length() - 5);
 				
 				try
 				{
