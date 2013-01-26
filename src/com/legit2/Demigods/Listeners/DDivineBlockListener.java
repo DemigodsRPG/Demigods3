f0b3bed127778cb9db47b7daa921cdf5b6681803
Alex Bennett <me@alexben.net>
2013-01-26 02:32:03 -0600

    Testing stuff.

--- src/com/legit2/Demigods/Listeners/DDivineBlockListener.java
+++ src/com/legit2/Demigods/Listeners/DDivineBlockListener.java
@@ -184,7 +184,8 @@ public class DDivineBlockListener implements Listener
 		for(Location divineBlock : DDivineBlocks.getAllShrines())
 		{
 			OfflinePlayer charOwner = DCharUtil.getOwner(DDivineBlocks.getShrineOwner(divineBlock));
-
+			DMiscUtil.serverMsg("Shrine Owner: " + DDivineBlocks.getShrineOwner(divineBlock));
+			
 			// Check for world errors
 			if(!divineBlock.getWorld().equals(event.getPlayer().getWorld())) return;
 			if(event.getFrom().getWorld() != divineBlock.getWorld()) return;
