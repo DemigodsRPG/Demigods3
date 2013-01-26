0b9d2febd29f32f3a15639a45e6b8d3fe0dd47da
Alex Bennett <me@alexben.net>
2013-01-26 00:36:53 -0600

    Reimplemented Weiner-Checks.

--- src/com/legit2/Demigods/Listeners/DDivineBlockListener.java
+++ src/com/legit2/Demigods/Listeners/DDivineBlockListener.java
@@ -242,15 +242,22 @@ public class DDivineBlockListener implements Listener
 	{		
 		List<Block> blocks = event.getBlocks();
 		
+		DMiscUtil.serverMsg("Weiner 1");
 		CHECKBLOCKS:
 		for(Block block : blocks)
 		{
 			try
 			{
+				DMiscUtil.serverMsg("Weiner 2");
+
 				for(Location divineBlock : DDivineBlocks.getAllDivineBlocks())
 				{
+					DMiscUtil.serverMsg("Weiner 3");
+
 					if(block.getLocation().equals(divineBlock))
 					{
+						DMiscUtil.serverMsg("Weiner 4");
+
 						event.setCancelled(true);
 						break CHECKBLOCKS;
 					}
