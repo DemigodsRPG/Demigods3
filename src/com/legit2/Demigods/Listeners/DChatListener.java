c72c6941e4e37651024182bd6ed913114c80f0f3
HmmmQuestionMark <support@clashnia.com>
2013-02-07 07:57:14 -0500

    Update chat listener to fit with new characters.

--- src/com/legit2/Demigods/Listeners/DChatListener.java
+++ src/com/legit2/Demigods/Listeners/DChatListener.java
@@ -101,7 +101,6 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 
-import com.legit2.Demigods.Libraries.DCharacter;
 import com.legit2.Demigods.Utilities.DDataUtil;
 import com.legit2.Demigods.Utilities.DPlayerUtil;
 import com.legit2.Demigods.Utilities.DMiscUtil;
@@ -114,9 +113,6 @@ public class DChatListener implements Listener
 		// Define variables
 		Player player = event.getPlayer();
 		Set<Player> viewing = event.getRecipients();
-		DCharacter character = DPlayerUtil.getCurrentChar(player);
-		
-		if(!character.isImmortal()) return;
 		
 		if(event.getMessage().equals("pl")) pl(player,event);
 		
@@ -134,15 +130,10 @@ public class DChatListener implements Listener
 		
 		for(Player onlinePlayer : DMiscUtil.getPlugin().getServer().getOnlinePlayers())
 		{
-			DCharacter character = DPlayerUtil.getCurrentChar(player);
 			String alliance = DPlayerUtil.getCurrentAlliance(player);
 
-			if(character.isImmortal())
-			{
-				if(!alliances.containsKey(alliance.toUpperCase())) alliances.put(alliance.toUpperCase(), new ArrayList<String>());
-				
-				alliances.get(alliance.toUpperCase()).add(onlinePlayer.getName());
-			}
+			if(!alliances.containsKey(alliance.toUpperCase())) alliances.put(alliance.toUpperCase(), new ArrayList<String>());
+			alliances.get(alliance.toUpperCase()).add(onlinePlayer.getName());
 		}
 	
 		for(String alliance : alliances.keySet())
