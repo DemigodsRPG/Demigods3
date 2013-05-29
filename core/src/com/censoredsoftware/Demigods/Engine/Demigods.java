1eccb0e841b4571cd0af481da6a341dd0194f91f
Alex Bennett <me@alexben.net>
2013-05-28 21:39:15 -0500

    Testing Stuff: Part III, The Sequel. Penis.

--- core/src/com/censoredsoftware/Demigods/Engine/Demigods.java
+++ core/src/com/censoredsoftware/Demigods/Engine/Demigods.java
@@ -321,9 +321,7 @@ class Commands implements CommandExecutor
 		Player player = (Player) sender;
 		Location location = player.getTargetBlock(null, 50).getLocation().add(0, 1, 0);
 
-		location.getWorld().spawnEntity(location, EntityType.EXPERIENCE_ORB).getLocation().toVector().zero();
-		location.getWorld().spawnEntity(location, EntityType.FIREBALL).getLocation().toVector().zero();
-		location.getWorld().spawnEntity(location, EntityType.FIREWORK).getLocation().toVector().zero();
+		location.getWorld().spawnEntity(location, EntityType.ENDER_PEARL).getLocation().toVector().zero();
 
 		return true;
 	}
