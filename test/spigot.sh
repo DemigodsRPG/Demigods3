f41a13feefc4254786dd67fea65bf46427971731
HmmmQuestionMark <support@clashnia.com>
2013-07-02 21:43:20 -0400

    Maybe this will do something.

--- test/spigot.sh
+++ test/spigot.sh
@@ -1,3 +1,3 @@
 /usr/bin/env sh
 
-mkdir plugins | cp ./target/Demigods-3.0.0-DEVELOPMENT.jar ./plugins/Demigods.jar | wget http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar | java -Xmx300M -Xms300M -jar spigot.jar
+mkdir plugins | cp ./target/Demigods-3.0.0-DEVELOPMENT.jar ./plugins/Demigods.jar | wget http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar | java -Xmx100M -Xms100M -jar spigot.jar
