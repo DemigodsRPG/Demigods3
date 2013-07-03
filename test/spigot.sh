41791fdf4184d3310cfde772ee5a5048f869f982
HmmmQuestionMark <support@clashnia.com>
2013-07-02 21:48:03 -0400

    That didn't do anything. Reverting.

--- test/spigot.sh
+++
@@ -1,3 +0,0 @@
-/usr/bin/env sh
-
-mkdir plugins | cp ./target/Demigods-3.0.0-DEVELOPMENT.jar ./plugins/Demigods.jar | wget http://ci.md-5.net/job/Spigot/lastSuccessfulBuild/artifact/Spigot-Server/target/spigot.jar | java -Xmx100M -Xms100M -jar spigot.jar
