8dad18d5dbcbc305ccb431459b71014f89fc084b
HmmmQuestionMark <support@clashnia.com>
2013-07-05 15:06:46 -0400

    Too big.

--- src/main/java/com/censoredsoftware/Demigods/Episodes/Demo/Deity/Donator/Disco.java
+++ src/main/java/com/censoredsoftware/Demigods/Episodes/Demo/Deity/Donator/Disco.java
@@ -99,9 +99,9 @@ class RainbowWalking extends Ability
 
 	private static void rainbow(Player player)
 	{
-		for(int x = -2; x < 2; x++)
+		for(int x = -1; x < 1; x++)
 		{
-			for(int z = -2; z < 2; z++)
+			for(int z = -1; z < 1; z++)
 			{
 				player.sendBlockChange(player.getLocation().getBlock().getRelative(BlockFace.DOWN).getLocation().add(x, 0, z), Material.WOOL, (byte) MiscUtility.generateIntRange(0, 15));
 			}
