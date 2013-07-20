7ec30cd39d6ac92f24ddc71da646eca701a50416
Alex Bennett <me@alexben.net>
2013-07-20 04:23:47 -0500

    Removed old warp/invite stuff.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Object/General/DemigodsLocation.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Object/General/DemigodsLocation.java
@@ -1,14 +1,11 @@
 package com.censoredsoftware.Demigods.Engine.Object.General;
 
-import java.util.Map;
 import java.util.Set;
 
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 
 import redis.clients.johm.*;
-
-import com.censoredsoftware.Demigods.Engine.Object.Player.PlayerCharacter;
 
 @Model
 public class DemigodsLocation
@@ -131,111 +128,5 @@ public class DemigodsLocation
 	public static Set<DemigodsLocation> getAllLocations()
 	{
 		return DemigodsLocation.loadAll();
-	}
-
-	/**
-	 * Checks to see if <code>character</code> has a warp for <code>altar</code>.
-	 * 
-	 * @param character the character to be checked.
-	 * @return true/false depending on if the <code>character</code> has the warp.
-	 */
-	public static boolean hasWarp(PlayerCharacter character) // TODO Move all warps and invites to PlayerCharacterMeta.
-	{
-		// TODO Fix warps.
-		return false;
-	}
-
-	/**
-	 * Returns true if <code>character</code> has invites in their que.
-	 * 
-	 * @param character the character to check.
-	 * @return true/false depending on the presence of invites.
-	 */
-	public static boolean hasInvites(PlayerCharacter character)
-	{
-		return !character.getInvites().isEmpty();
-	}
-
-	/**
-	 * Gets the invite from <code>inviting</code> to <code>invited</code> and
-	 * returns a <code>DemigodsLocation</code>.
-	 * 
-	 * @param inviting the character who sent the invite.
-	 * @param invited the character who received the invite.
-	 * @return the <code>DemigodsLocation</code> of the invite.
-	 */
-	public static DemigodsLocation getInvite(PlayerCharacter inviting, PlayerCharacter invited)
-	{
-		if(hasInvites(invited))
-		{
-			for(Map.Entry<DemigodsLocation, String> invite : invited.getInvites().entrySet())
-			{
-				if(invite.getValue().equalsIgnoreCase(inviting.getName())) return invite.getKey();
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Returns the invite for the <code>character</code> with the name <code>name</code>.
-	 * 
-	 * @param character the character to check.
-	 * @param name the name of the invite to get.
-	 * @return the <code>DemigodsLocation</code> of the invite.
-	 */
-	public static DemigodsLocation getInvite(PlayerCharacter character, String name)
-	{
-		if(hasInvites(character))
-		{
-			for(Map.Entry<DemigodsLocation, String> invite : character.getInvites().entrySet())
-			{
-				if(invite.getValue().equalsIgnoreCase(name)) return invite.getKey();
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Returns true if <code>invited</code> has already been given an invite from <code>inviting</code>.
-	 * 
-	 * @param inviting the character the invite is from.
-	 * @param invited the character the invite is to.
-	 * @return boolean for if the character has been invited or not.
-	 */
-	public static boolean alreadyInvited(PlayerCharacter inviting, PlayerCharacter invited)
-	{
-		return getInvite(inviting, invited) != null;
-	}
-
-	/**
-	 * Sends an invite to <code>to</code> from <code>from</code>.
-	 * 
-	 * @param from the character whom the invite is from.
-	 * @param to the character whom the invite is to.
-	 */
-	public static void addInvite(PlayerCharacter from, PlayerCharacter to)
-	{
-		to.addInvite(create(from.getOfflinePlayer().getPlayer().getLocation()), from.getName());
-	}
-
-	/**
-	 * Removes <code>invite</code> from <code>invited</code>.
-	 * 
-	 * @param invited the character to remove the invite from.
-	 * @param invite the invite to remove.
-	 */
-	public static void removeInvite(PlayerCharacter invited, DemigodsLocation invite)
-	{
-		invited.removeInvite(invite);
-	}
-
-	/**
-	 * Clears all invited for <code>character</code>.
-	 * 
-	 * @param character the character whose invites to remove.
-	 */
-	public static void clearInvites(PlayerCharacter character)
-	{
-		character.clearInvites();
 	}
 }
