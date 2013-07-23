2b270f8e092a8d7c3ce80ff6dc49032a5ed042ea
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-22 20:13:00 -0400

    A few changes.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Listener/BattleListener.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Listener/BattleListener.java
@@ -14,33 +14,32 @@ import org.bukkit.event.player.PlayerTeleportEvent;
 import com.censoredsoftware.Demigods.Engine.Demigods;
 import com.censoredsoftware.Demigods.Engine.Object.Battle.Battle;
 import com.censoredsoftware.Demigods.Engine.Object.Battle.BattleParticipant;
-import com.censoredsoftware.Demigods.Engine.Utility.BattleUtility;
 
 public class BattleListener implements Listener
 {
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public static void onDamageBy(EntityDamageByEntityEvent event)
 	{
-		if(!BattleUtility.canParticipate(event.getEntity()) || !BattleUtility.canParticipate(event.getDamager())) return;
+		if(!Battle.canParticipate(event.getEntity()) || !Battle.canParticipate(event.getDamager())) return;
 
 		// Define participants
-		BattleParticipant damageeParticipant = BattleUtility.defineParticipant(event.getEntity());
-		BattleParticipant damagerParticipant = BattleUtility.defineParticipant(event.getDamager());
+		BattleParticipant damageeParticipant = Battle.defineParticipant(event.getEntity());
+		BattleParticipant damagerParticipant = Battle.defineParticipant(event.getDamager());
 
 		// Calculate midpoint location
 		Location midpoint = damagerParticipant.getCurrentLocation().toVector().getMidpoint(event.getEntity().getLocation().toVector()).toLocation(damagerParticipant.getCurrentLocation().getWorld());
 
-		if(!BattleUtility.existsNear(midpoint) && !BattleUtility.existsInRadius(midpoint))
+		if(!Battle.existsNear(midpoint) && !Battle.existsInRadius(midpoint))
 		{
 			// Create new battle
 			Battle battle = Battle.create(damagerParticipant, damageeParticipant);
 
 			// Teleport if needed
-			BattleUtility.teleportIfNeeded(damageeParticipant, battle);
-			BattleUtility.teleportIfNeeded(damagerParticipant, battle);
+			Battle.teleportIfNeeded(damageeParticipant, battle);
+			Battle.teleportIfNeeded(damagerParticipant, battle);
 
 			// Battle death
-			if(event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(BattleUtility.battleDeath(damagerParticipant, damageeParticipant, battle));
+			if(event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(Battle.battleDeath(damagerParticipant, damageeParticipant, battle));
 
 			// Debug
 			Demigods.message.broadcast(ChatColor.YELLOW + "Battle started involving " + damagerParticipant.getRelatedCharacter().getName() + " and " + damageeParticipant.getRelatedCharacter().getName() + "!");
@@ -48,56 +47,56 @@ public class BattleListener implements Listener
 		else
 		{
 			// Add to existing battle
-			Battle battle = BattleUtility.getInRadius(midpoint);
+			Battle battle = Battle.getInRadius(midpoint);
 
 			// Teleport if needed
-			BattleUtility.teleportIfNeeded(damageeParticipant, battle);
-			BattleUtility.teleportIfNeeded(damagerParticipant, battle);
+			Battle.teleportIfNeeded(damageeParticipant, battle);
+			Battle.teleportIfNeeded(damagerParticipant, battle);
 
 			// Add participants from this event
 			battle.getMeta().addParticipant(damageeParticipant);
 			battle.getMeta().addParticipant(damagerParticipant);
 
 			// Battle death
-			if(event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(BattleUtility.battleDeath(damagerParticipant, damageeParticipant, battle));
+			if(event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(Battle.battleDeath(damagerParticipant, damageeParticipant, battle));
 		}
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onDamage(EntityDamageEvent event)
 	{
-		if(event instanceof EntityDamageByEntityEvent || !BattleUtility.canParticipate(event.getEntity())) return;
+		if(event instanceof EntityDamageByEntityEvent || !Battle.canParticipate(event.getEntity())) return;
 
-		BattleParticipant participant = BattleUtility.defineParticipant(event.getEntity());
+		BattleParticipant participant = Battle.defineParticipant(event.getEntity());
 
 		// Battle death
-		if(BattleUtility.isInBattle(participant) && event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(BattleUtility.battleDeath(participant, BattleUtility.getBattle(participant)));
+		if(Battle.isInBattle(participant) && event.getDamage() >= ((LivingEntity) event.getEntity()).getHealth()) event.setCancelled(Battle.battleDeath(participant, Battle.getBattle(participant)));
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBattleMove(PlayerMoveEvent event)
 	{
-		if(!BattleUtility.canParticipate(event.getPlayer())) return;
-		BattleParticipant participant = BattleUtility.defineParticipant(event.getPlayer());
+		if(!Battle.canParticipate(event.getPlayer())) return;
+		BattleParticipant participant = Battle.defineParticipant(event.getPlayer());
 		event.setCancelled(onBattleMove(event.getTo(), event.getFrom(), participant));
 	}
 
 	@EventHandler(priority = EventPriority.HIGHEST)
 	public void onBattleMove(PlayerTeleportEvent event)
 	{
-		if(!BattleUtility.canParticipate(event.getPlayer())) return;
-		BattleParticipant participant = BattleUtility.defineParticipant(event.getPlayer());
+		if(!Battle.canParticipate(event.getPlayer())) return;
+		BattleParticipant participant = Battle.defineParticipant(event.getPlayer());
 		event.setCancelled(onBattleMove(event.getTo(), event.getFrom(), participant));
 	}
 
 	private static boolean onBattleMove(Location toLocation, Location fromLocation, BattleParticipant participant)
 	{
-		boolean to = BattleUtility.existsInRadius(toLocation);
-		boolean from = BattleUtility.existsInRadius(fromLocation);
+		boolean to = Battle.existsInRadius(toLocation);
+		boolean from = Battle.existsInRadius(fromLocation);
 		boolean enter = to == true && from == false;
 		boolean exit = to == false && from == true;
-		if(enter) BattleUtility.getInRadius(toLocation).getMeta().addParticipant(participant);
-		if(exit && BattleUtility.isInBattle(participant)) return true;
+		if(enter) Battle.getInRadius(toLocation).getMeta().addParticipant(participant);
+		if(exit && Battle.isInBattle(participant)) return true;
 		return false;
 	}
 }
--- src/main/java/com/censoredsoftware/Demigods/Engine/Runnable/BattleRunnable.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Runnable/BattleRunnable.java
@@ -1,19 +1,27 @@
 package com.censoredsoftware.Demigods.Engine.Runnable;
 
+import org.bukkit.Bukkit;
 import org.bukkit.scheduler.BukkitRunnable;
 
 import com.censoredsoftware.Demigods.Engine.Object.Battle.Battle;
-import com.censoredsoftware.Demigods.Engine.Utility.BattleUtility;
 
 public class BattleRunnable extends BukkitRunnable
 {
 	@Override
 	public void run()
 	{
+		// Battle onTick logic
 		for(Battle battle : Battle.getAll()) // TODO Something is buggy here.
 		{
 			if(battle.getMeta().getKills() > battle.getMaxKills() || battle.getStartTime() + battle.getDuration() <= System.currentTimeMillis() && battle.getMeta().getKills() > battle.getMinKills()) battle.end();
-			else BattleUtility.battleBorder(battle);
+			else Battle.battleBorder(battle);
+		}
+
+		// Delete old battles
+		if(Battle.battleQueue.size() >= (int) Math.ceil(Bukkit.getOnlinePlayers().length / 2.0))
+		{
+			Battle delete = Battle.battleQueue.poll();
+			if(delete != null) delete.delete();
 		}
 	}
 }
