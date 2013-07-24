19274aa3561e802835c95ac4e58495c938182613
Alex Bennett <me@alexben.net>
2013-07-24 04:40:11 -0500

    Just experimenting now.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Listener/InventoryListener.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Listener/InventoryListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.inventory.InventoryClickEvent;
+import org.bukkit.inventory.PlayerInventory;
 
 import com.censoredsoftware.Demigods.Engine.Demigods;
 import com.censoredsoftware.Demigods.Engine.Object.Ability.AbilityBind;
@@ -19,18 +20,20 @@ public class InventoryListener implements Listener
 		Player player = (Player) event.getWhoClicked();
 		PlayerCharacter character = PlayerWrapper.getPlayer(player).getCurrent();
 
+		if(event.getClickedInventory() instanceof PlayerInventory)
+		{
+			PlayerInventory inventory = (PlayerInventory) event.getClickedInventory();
+			Demigods.message.broadcast("Clicked slot: " + event.getSlot() + "/" + event.getRawSlot());
+			Demigods.message.broadcast("Item: " + inventory.getItem(event.getSlot()));
+			Demigods.message.broadcast("Item (Raw Slot): " + inventory.getItem(event.getRawSlot()));
+		}
+
 		// Return if no character exists
 		if(character == null) return;
 
 		for(AbilityBind bind : character.getMeta().getBinds())
 		{
-			// Set<Integer> hotBar = Ranges.closed(event.getClickedInventory().getSize(), event.getClickedInventory().getSize() + 9).asSet(DiscreteDomains.integers());
-
-			Demigods.message.broadcast("Range: " + event.getInventory().getSize() + ", " + (event.getClickedInventory().getSize() + 9));
-			Demigods.message.broadcast("Slot: " + (event.getSlot() - 9));
-			Demigods.message.broadcast("Raw Slot: " + event.getRawSlot());
-
-			if(bind.getSlot() == (event.getSlot() - 9)) event.setCancelled(true);
+			if(event.getCurrentItem().isSimilar(bind.getItem().toItemStack())) event.setCancelled(true);
 		}
 	}
 }
