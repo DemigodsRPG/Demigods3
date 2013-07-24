491a4b24d15d697f5078cae8a91a798d7b58c552
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-24 00:16:03 -0400

    Attempt to stop error.

--- src/main/java/com/censoredsoftware/Demigods/Episodes/Demo/Structure/Obelisk.java
+++ src/main/java/com/censoredsoftware/Demigods/Episodes/Demo/Structure/Obelisk.java
@@ -64,30 +64,73 @@ public class Obelisk implements StructureInfo
 				}));
 
 				// Everything else.
-				add(new StructureSchematic(0, 0, -1, 0, 3, -1, new ArrayList<StructureBlockData>()
+				add(new StructureSchematic(0, 0, -1, 0, 2, -1, new ArrayList<StructureBlockData>()
 				{
 					{
 						add(new StructureBlockData(Material.SMOOTH_BRICK));
 					}
 				}));
-				add(new StructureSchematic(0, 0, 1, 0, 3, 1, new ArrayList<StructureBlockData>()
+				add(new StructureSchematic(0, 0, 1, 0, 2, 1, new ArrayList<StructureBlockData>()
 				{
 					{
 						add(new StructureBlockData(Material.SMOOTH_BRICK));
 					}
 				}));
-				add(new StructureSchematic(1, 0, 0, 1, 3, 0, new ArrayList<StructureBlockData>()
+				add(new StructureSchematic(1, 0, 0, 1, 2, 0, new ArrayList<StructureBlockData>()
 				{
 					{
 						add(new StructureBlockData(Material.SMOOTH_BRICK));
 					}
 				}));
-				add(new StructureSchematic(-1, 0, 0, -1, 3, 0, new ArrayList<StructureBlockData>()
+				add(new StructureSchematic(-1, 0, 0, -1, 2, 0, new ArrayList<StructureBlockData>()
 				{
 					{
 						add(new StructureBlockData(Material.SMOOTH_BRICK));
 					}
 				}));
+				add(new StructureSchematic(0, 3, 0, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_BLOCK));
+					}
+				}));
+				add(new StructureSchematic(0, 4, 0, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_BLOCK));
+					}
+				}));
+				add(new StructureSchematic(0, 3, -1, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_LAMP_ON));
+					}
+				}));
+				add(new StructureSchematic(0, 3, 1, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_LAMP_ON));
+					}
+				}));
+				add(new StructureSchematic(1, 3, 0, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_LAMP_ON));
+					}
+				}));
+				add(new StructureSchematic(-1, 3, 0, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_LAMP_ON));
+					}
+				}));
+				add(new StructureSchematic(0, 5, 0, new ArrayList<StructureBlockData>()
+				{
+					{
+						add(new StructureBlockData(Material.REDSTONE_LAMP_ON));
+					}
+				}));
+
 			}
 		};
 	}
