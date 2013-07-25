26e2e1d003cc5f3f8cf813169dc85cb2ad1b064a
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-25 00:21:55 -0400

    Pinpoint lag.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Demigods.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Demigods.java
@@ -14,7 +14,6 @@ import com.censoredsoftware.Demigods.Engine.Command.GeneralCommands;
 import com.censoredsoftware.Demigods.Engine.Command.MainCommand;
 import com.censoredsoftware.Demigods.Engine.Conversation.Conversation;
 import com.censoredsoftware.Demigods.Engine.Exceptions.DemigodsStartupException;
-import com.censoredsoftware.Demigods.Engine.Listener.GriefListener;
 import com.censoredsoftware.Demigods.Engine.Listener.StructureListener;
 import com.censoredsoftware.Demigods.Engine.Module.ConfigModule;
 import com.censoredsoftware.Demigods.Engine.Module.MessageModule;
@@ -167,7 +166,7 @@ public class Demigods
 		// instance.getServer().getPluginManager().registerEvents(new BattleListener(), instance);
 		// instance.getServer().getPluginManager().registerEvents(new CommandListener(), instance);
 		// instance.getServer().getPluginManager().registerEvents(new EntityListener(), instance);
-		instance.getServer().getPluginManager().registerEvents(new GriefListener(), instance);
+		// instance.getServer().getPluginManager().registerEvents(new GriefListener(), instance);
 		// instance.getServer().getPluginManager().registerEvents(new InventoryListener(), instance);
 		// instance.getServer().getPluginManager().registerEvents(new PlayerListener(), instance);
 		instance.getServer().getPluginManager().registerEvents(new StructureListener(), instance);
