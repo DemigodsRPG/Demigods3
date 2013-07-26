eedff272ea8d5989ad20744c3f210a16e016d3ee
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-26 19:40:14 -0400

    Try this.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Object/Region.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Object/Region.java
@@ -43,15 +43,9 @@ public class Region
 
 		private static int getRegionCoordinate(int number)
 		{
-			if(number % 256 == 0) return number;
-			boolean positive = number > 0;
-			int half = positive ? 128 : -128;
-			int round = ((number + half) / 256);
-			if(round == -1) return -256;
-			if(round == 0) return 0;
-			if(round == 1) return 256;
-			if(number % 256 > half) return 256 * round;
-			return 256 * (round + (positive ? 1 : -1));
+			int temp = number % 256;
+			if(temp < 128) return number - temp;
+			return number + 256 - temp;
 		}
 	}
 }
