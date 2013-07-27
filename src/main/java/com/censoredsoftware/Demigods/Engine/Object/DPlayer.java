531eb0792e0177544954ca011b070fb240020e2a
Alex Bennett <me@alexben.net>
2013-07-27 05:37:18 -0500

    This can be async.

--- src/main/java/com/censoredsoftware/Demigods/Engine/Object/DPlayer.java
+++ src/main/java/com/censoredsoftware/Demigods/Engine/Object/DPlayer.java
@@ -72,7 +72,7 @@ public class DPlayer
 			int delay = Demigods.config.getSettingInt("zones.pvp_area_delay_time");
 			DataUtility.saveTimed(player.getName(), "pvp_cooldown", true, delay);
 
-			int taskId = Bukkit.getScheduler().scheduleSyncDelayedTask(Demigods.plugin, new Runnable()
+			int taskId = Bukkit.getScheduler().scheduleAsyncDelayedTask(Demigods.plugin, new Runnable()
 			{
 				@Override
 				public void run()
