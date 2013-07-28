80b0dd5aaa5f012661b36fa9c003885c7f8430a2
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-27 22:31:17 -0400

    Try this.

---
+++ src/main/java/com/censoredsoftware/Demigods/butt.java
@@ -0,0 +1,11 @@
+package com.censoredsoftware.Demigods;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Alex
+ * Date: 7/27/13
+ * Time: 10:31 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class butt {
+}
--- src/main/java/com/censoredsoftware/Demigods/engine/exception/DemigodsStartupException.java
+++
@@ -1,9 +0,0 @@
-package com.censoredsoftware.demigods.engine.exception;
-
-public class DemigodsStartupException extends Exception
-{
-	public DemigodsStartupException()
-	{
-		super("demigods failed to initialize.");
-	}
-}
