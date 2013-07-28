a46445c294b4cea4f8731f4a2788d3127fa619a6
HmmmQuestionMark <ajchauncey@gmail.com>
2013-07-27 22:25:07 -0400

    Remove all.

--- src/main/java/com/censoredsoftware/Demigods/Engine/exception/SpigotNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package com.censoredsoftware.demigods.engine.exception;
-
-public class SpigotNotFoundException extends IllegalArgumentException
-{
-	public SpigotNotFoundException()
-	{
-		super("Spigot is not installed.");
-	}
-}
