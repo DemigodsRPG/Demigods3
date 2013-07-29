772fa8b9cb3a3ff4364fc04261afb0b7d4375865
Alex Bennett <me@alexben.net>
2013-07-28 22:42:08 -0500

    Disable liquid move event listener.

--- src/main/java/com/censoredsoftware/demigods/engine/listener/GriefListener.java
+++ src/main/java/com/censoredsoftware/demigods/engine/listener/GriefListener.java
@@ -2,10 +2,9 @@ package com.censoredsoftware.demigods.engine.listener;
 
 // TODO Fix for lag.
 
-import com.censoredsoftware.demigods.engine.element.Structure;
-import com.censoredsoftware.demigods.engine.location.DLocation;
-import com.censoredsoftware.demigods.engine.player.DCharacter;
-import com.censoredsoftware.demigods.engine.player.DPlayer;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.block.Block;
@@ -20,8 +19,10 @@ import org.bukkit.event.entity.EntityExplodeEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.HashSet;
-import java.util.Set;
+import com.censoredsoftware.demigods.engine.element.Structure;
+import com.censoredsoftware.demigods.engine.location.DLocation;
+import com.censoredsoftware.demigods.engine.player.DCharacter;
+import com.censoredsoftware.demigods.engine.player.DPlayer;
 
 public class GriefListener implements Listener
 {
@@ -105,7 +106,7 @@ public class GriefListener implements Listener
 		}
 	}
 
-	@EventHandler(priority = EventPriority.HIGHEST)
+	// TODO: @EventHandler(priority = EventPriority.HIGHEST)
 	public void onLiquidMove(BlockFromToEvent event)
 	{
 		if(Structure.Util.isInRadiusWithFlag(event.getToBlock().getLocation(), Structure.Flag.NO_GRIEFING, true)) event.setCancelled(true);
