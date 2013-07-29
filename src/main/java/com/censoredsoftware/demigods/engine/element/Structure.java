1611821437f74dbd074927afb6c3101c8209321a
HmmmQuestionMark <support@clashnia.com>
2013-07-29 04:20:11 -0400

    Fix check.

--- src/main/java/com/censoredsoftware/demigods/engine/element/Structure.java
+++ src/main/java/com/censoredsoftware/demigods/engine/element/Structure.java
@@ -315,7 +315,7 @@ public abstract class Structure
 		{
 			for(int i = 0; i < blockData.size(); i++)
 			{
-				if(Randoms.generateIntRange(0, (100 - blockData.get(i).getOdds() * (i + 1) / 3)) == 0) return blockData.get(i);
+				if(Randoms.generateIntRange(0, (100 - blockData.get(i).getOdds()) * (blockData.size() / (i + 1))) == 0) return blockData.get(i);
 			}
 			return blockData.get(0);
 		}
