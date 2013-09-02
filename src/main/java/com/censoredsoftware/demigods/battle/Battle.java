1b7f23c58b81941193b6858adad64d9e22da3cba
HmmmQuestionMark <ajchauncey@gmail.com>
2013-09-01 23:59:50 -0400

    Fix for battles?

--- src/main/java/com/censoredsoftware/demigods/battle/Battle.java
+++ src/main/java/com/censoredsoftware/demigods/battle/Battle.java
@@ -694,7 +694,7 @@ public class Battle implements ConfigurationSerializable
 		 */
 		public static boolean canTarget(Participant participant) // TODO REDO THIS
 		{
-			return participant.canPvp() || participant.getCurrentLocation() != null && !Structure.Util.isInRadiusWithFlag(participant.getCurrentLocation(), Structure.Flag.NO_PVP);
+			return participant == null || !(participant instanceof DCharacter || participant instanceof Pet) || participant.canPvp() || participant.getCurrentLocation() != null && !Structure.Util.isInRadiusWithFlag(participant.getCurrentLocation(), Structure.Flag.NO_PVP);
 		}
 
 		/**
