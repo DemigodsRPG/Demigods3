c19e8983065a2b320056039594a70adc3685ba72
HmmmQuestionMark <ajchauncey@gmail.com>
2013-09-03 13:57:41 -0400

    Does this look better?

--- src/main/java/com/censoredsoftware/demigods/player/DCharacter.java
+++ src/main/java/com/censoredsoftware/demigods/player/DCharacter.java
@@ -456,7 +456,7 @@ public class DCharacter implements Participant, ConfigurationSerializable
 
 	public void chatWithAlliance(String message)
 	{
-		sendAllianceMessage(" " + ChatColor.YELLOW + getAlliance() + "s " + ChatColor.DARK_GRAY + "" + Symbol.BLACK_FLAG + " " + getDeity().getColor() + name + ChatColor.GRAY + ": " + ChatColor.RESET + message);
+		sendAllianceMessage(" " + ChatColor.GRAY + getAlliance() + "s " + ChatColor.DARK_GRAY + "" + Symbol.BLACK_FLAG + " " + getDeity().getColor() + name + ChatColor.GRAY + ": " + ChatColor.RESET + message);
 		Messages.info("[" + getAlliance() + "]" + name + ": " + message);
 	}
 
