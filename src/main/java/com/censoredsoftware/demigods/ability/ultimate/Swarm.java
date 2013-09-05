34ac44faa1fc3d5d1994cc82c1a1252ebe9da04e
HmmmQuestionMark <ajchauncey@gmail.com>
2013-09-05 15:59:17 -0400

    Another fix.

--- src/main/java/com/censoredsoftware/demigods/ability/ultimate/Swarm.java
+++ src/main/java/com/censoredsoftware/demigods/ability/ultimate/Swarm.java
@@ -168,7 +168,7 @@ public class Swarm implements Ability
 			@EventHandler(priority = EventPriority.HIGHEST)
 			public void onEntityTargetEntity(EntityTargetLivingEntityEvent targetEvent)
 			{
-				if(Demigods.MiscUtil.isDisabledWorld(targetEvent.getEntity().getWorld()) || (targetEvent.getTarget() instanceof Player)) return;
+				if(Demigods.MiscUtil.isDisabledWorld(targetEvent.getEntity().getWorld()) || !(targetEvent.getTarget() instanceof Player)) return;
 				if(targetEvent.getEntity() instanceof Zombie && Deity.Util.canUseDeitySilent(DPlayer.Util.getPlayer((Player) targetEvent.getTarget()).getCurrent(), deity)) targetEvent.setCancelled(true);
 			}
 		};
