6cd5e4f2ed04bfe0511490d6ffbcb106df981c39
Alex <me@alexben.net>
2013-09-04 22:27:44 -0400

    Whoops.

--- src/main/java/com/censoredsoftware/demigods/player/DPlayer.java
+++ src/main/java/com/censoredsoftware/demigods/player/DPlayer.java
@@ -412,7 +412,6 @@ public class DPlayer implements ConfigurationSerializable
 			DataManager.removeTemp(player.getName(), "prayer_location");
 			DataManager.removeTimed(player.getName(), "currently_creating");
 			DataManager.removeTimed(player.getName(), "currently_forsaking");
-
 		}
 
 		/**
