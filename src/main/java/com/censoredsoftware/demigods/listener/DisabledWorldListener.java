665d6ed336bc126c614f8a8b6374aaa3842e3374
HmmmQuestionMark <ajchauncey@gmail.com>
2013-09-06 21:30:22 -0400

    More stuff.

--- src/main/java/com/censoredsoftware/demigods/listener/DisabledWorldListener.java
+++ src/main/java/com/censoredsoftware/demigods/listener/DisabledWorldListener.java
@@ -3,14 +3,14 @@ package com.censoredsoftware.demigods.listener;
 import com.censoredsoftware.demigods.Demigods;
 import com.censoredsoftware.demigods.player.DCharacter;
 import com.censoredsoftware.demigods.player.DPlayer;
-import org.bukkit.Bukkit;
+import com.censoredsoftware.demigods.util.Messages;
 import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerChangedWorldEvent;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.potion.PotionEffect;
 
 import java.util.Set;
 
@@ -29,46 +29,63 @@ public class DisabledWorldListener implements Listener
 	}
 
 	@EventHandler(priority = EventPriority.MONITOR)
-	public void onWorldSwitch(final PlayerChangedWorldEvent event)
+	public void onWorldSwitch(PlayerChangedWorldEvent event)
 	{
 		// Only continue if the player is a character
 		Player player = event.getPlayer();
+		DPlayer dPlayer = DPlayer.Util.getPlayer(player);
+		final DCharacter character = dPlayer.getCurrent();
 
-		if(!DPlayer.Util.isImmortal(player)) return;
+		if(dPlayer.getCurrent() == null) return;
 
-		// Finalize things for the delay
-		final DCharacter character = DPlayer.Util.getPlayer(player).getCurrent();
-		final String playerName = player.getName();
-
-		Bukkit.getScheduler().scheduleSyncDelayedTask(Demigods.PLUGIN, new BukkitRunnable()
+		// Leaving a disabled world
+		if(Demigods.MiscUtil.isDisabledWorld(event.getFrom()) && !Demigods.MiscUtil.isDisabledWorld(player.getWorld()))
 		{
-			@Override
-			public void run()
+			dPlayer.setDisabledWorldInventory(player);
+			player.setDisplayName(character.getDeity().getColor() + character.getName());
+			try
 			{
-				if(!Bukkit.getOfflinePlayer(playerName).isOnline()) return; // Oh shit, not sure how to prevent this.
-
-				// Redefine stuff
-				Player player = Bukkit.getOfflinePlayer(playerName).getPlayer();
-				DPlayer dPlayer = DPlayer.Util.getPlayer(playerName);
-
-				// Leaving a disabled world
-				if(Demigods.MiscUtil.isDisabledWorld(event.getFrom()) && !Demigods.MiscUtil.isDisabledWorld(player.getWorld()))
-				{
-					dPlayer.setDisabledWorldInventory(player);
-					character.getInventory().setToPlayer(player);
-					player.setDisplayName(character.getDeity().getColor() + character.getName());
-					player.setPlayerListName(character.getDeity().getColor() + character.getName());
-					player.sendMessage(ChatColor.YELLOW + "Demigods is enabled in this world.");
-				}
-				else if(!Demigods.MiscUtil.isDisabledWorld(event.getFrom()) && Demigods.MiscUtil.isDisabledWorld(player.getWorld()))
-				{
-					character.saveInventory();
-					dPlayer.applyDisabledWorldInventory();
-					player.setDisplayName(player.getName());
-					player.setPlayerListName(player.getName());
-					player.sendMessage(ChatColor.GRAY + "Demigods is disabled in this world.");
-				}
+				player.setPlayerListName(character.getDeity().getColor() + character.getName());
 			}
-		}, 60); // Let's see if it even tries at all.
+			catch(Exception e)
+			{
+				Messages.warning("Character name too long.");
+			}
+			player.setMaxHealth(character.getMaxHealth());
+			player.setHealth(character.getHealth() >= character.getMaxHealth() ? character.getMaxHealth() : character.getHealth());
+			player.setFoodLevel(character.getHunger());
+			player.setExp(character.getExperience());
+			player.setLevel(character.getLevel());
+			for(PotionEffect potion : player.getActivePotionEffects())
+				player.removePotionEffect(potion.getType());
+			if(character.getPotionEffects() != null) player.addPotionEffects(character.getPotionEffects());
+			player.setBedSpawnLocation(character.getBedSpawn());
+			character.getInventory().setToPlayer(player);
+			player.setDisplayName(character.getDeity().getColor() + character.getName());
+			player.setPlayerListName(character.getDeity().getColor() + character.getName());
+			player.sendMessage(ChatColor.YELLOW + "Demigods is enabled in this world.");
+		}
+		else if(!Demigods.MiscUtil.isDisabledWorld(event.getFrom()) && Demigods.MiscUtil.isDisabledWorld(player.getWorld()))
+		{
+			character.setHealth(player.getHealth() >= character.getMaxHealth() ? character.getMaxHealth() : player.getHealth());
+			character.setHunger(player.getFoodLevel());
+			character.setLevel(player.getLevel());
+			character.setExperience(player.getExp());
+			character.setLocation(player.getLocation());
+			if(player.getBedSpawnLocation() != null) character.setBedSpawn(player.getBedSpawnLocation());
+			character.setPotionEffects(player.getActivePotionEffects());
+			character.saveInventory();
+			player.setMaxHealth(20.0);
+			player.setHealth(20.0);
+			player.setFoodLevel(20);
+			player.setExp(0);
+			player.setLevel(0);
+			for(PotionEffect potion : player.getActivePotionEffects())
+				player.removePotionEffect(potion.getType());
+			dPlayer.applyDisabledWorldInventory();
+			player.setDisplayName(player.getName());
+			player.setPlayerListName(player.getName());
+			player.sendMessage(ChatColor.GRAY + "Demigods is disabled in this world.");
+		}
 	}
 }
