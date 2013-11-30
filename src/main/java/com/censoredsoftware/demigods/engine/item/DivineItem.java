7b67a2fb6710f00e97db7e76e58c47819fc1a511
HmmmQuestionMark <ajchauncey@gmail.com>
2013-11-30 16:49:43 -0500

    Remove the buttsword.

--- src/main/java/com/censoredsoftware/demigods/engine/item/DivineItem.java
+++ src/main/java/com/censoredsoftware/demigods/engine/item/DivineItem.java
@@ -2,47 +2,40 @@ package com.censoredsoftware.demigods.engine.item;
 
 import com.censoredsoftware.demigods.engine.item.book.BookOfPrayer;
 import com.censoredsoftware.demigods.engine.item.book.WelcomeBook;
-import com.censoredsoftware.demigods.engine.item.weapon.ButtSword;
-import com.censoredsoftware.demigods.engine.item.weapon.DeathBow;
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
 
-public enum DivineItem
-{
-	/**
-	 * Books
-	 */
-	BOOK_OF_PRAYER(BookOfPrayer.book, BookOfPrayer.recipe, BookOfPrayer.listener), WELCOME_BOOK(WelcomeBook.book, null, null),
+public enum DivineItem {
+    /**
+     * Books
+     */
+    BOOK_OF_PRAYER(BookOfPrayer.book, BookOfPrayer.recipe, BookOfPrayer.listener), WELCOME_BOOK(WelcomeBook.book, null, null);
 
-	/**
-	 * Weapons
-	 */
-	BUTT_SWORD(ButtSword.buttSword, ButtSword.recipe, ButtSword.listener), DEATH_BOW(DeathBow.deathBow, DeathBow.recipe, DeathBow.listener);
+    /**
+     * Weapons
+     */
+    // BUTT_SWORD(ButtSword.buttSword, ButtSword.recipe, ButtSword.listener), DEATH_BOW(DeathBow.deathBow, DeathBow.recipe, DeathBow.listener);
 
-	private final ItemStack item;
-	private final Recipe recipe;
-	private final Listener listener;
+    private final ItemStack item;
+    private final Recipe recipe;
+    private final Listener listener;
 
-	private DivineItem(ItemStack item, Recipe recipe, Listener listener)
-	{
-		this.item = item;
-		this.recipe = recipe;
-		this.listener = listener;
-	}
+    private DivineItem(ItemStack item, Recipe recipe, Listener listener) {
+        this.item = item;
+        this.recipe = recipe;
+        this.listener = listener;
+    }
 
-	public ItemStack getItem()
-	{
-		return item;
-	}
+    public ItemStack getItem() {
+        return item;
+    }
 
-	public Recipe getRecipe()
-	{
-		return recipe;
-	}
+    public Recipe getRecipe() {
+        return recipe;
+    }
 
-	public Listener getUniqueListener()
-	{
-		return listener;
-	}
+    public Listener getUniqueListener() {
+        return listener;
+    }
 }
