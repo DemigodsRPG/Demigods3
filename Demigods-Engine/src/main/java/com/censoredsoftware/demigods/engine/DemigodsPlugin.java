b5e8b49991dca43a861a08ccf96cff784440a97d
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-24 20:58:07 -0500

    Fixed a few messages.

--- Demigods-Engine/src/main/java/com/censoredsoftware/demigods/engine/DemigodsPlugin.java
+++ Demigods-Engine/src/main/java/com/censoredsoftware/demigods/engine/DemigodsPlugin.java
@@ -48,7 +48,7 @@ public class DemigodsPlugin extends CensoredJavaPlugin
 		}
 
 		// Print success!
-		enableMessage();
+		message("enabled");
 	}
 
 	/**
@@ -77,41 +77,22 @@ public class DemigodsPlugin extends CensoredJavaPlugin
 			ThreadManager.stopThreads();
 			HandlerList.unregisterAll(this);
 			Demigods.unloadPermissions();
-
-			Messages.info("Successfully disabled.");
 		}
 		catch(Throwable ignored)
 		{}
+
+		message("disabled");
 	}
 
-	private void enableMessage()
+	private void message(String status)
 	{
-		getLogger().info("  ");
 		getLogger().info("     ____            _           _");
 		getLogger().info("    |    \\ ___ _____|_|___ ___ _| |___");
 		getLogger().info("    |  |  | -_|     | | . | . | . |_ -|");
 		getLogger().info("    |____/|___|_|_|_|_|_  |___|___|___|");
 		getLogger().info("        Battle of the |___| Chosen");
 		getLogger().info("  ");
-		getLogger().info("  ");
-		getLogger().info("   _   _ _____    333333333333333");
-		getLogger().info("  | | | | ___ |  3:::::::::::::::33");
-		getLogger().info("   \\ V /| ____|  3::::::33333::::::3");
-		getLogger().info("    \\_/ |_____)  3333333     3:::::3");
-		getLogger().info("             _               3:::::3");
-		getLogger().info("            (_)              3:::::3");
-		getLogger().info("    ____ ___ _       33333333:::::3");
-		getLogger().info("   / ___)___) |      3:::::::::::3");
-		getLogger().info("  | |  |___ | |      33333333:::::3");
-		getLogger().info("  |_|  (___/|_|              3:::::3");
-		getLogger().info("                             3:::::3");
-		getLogger().info("                             3:::::3");
-		getLogger().info("    ___  ____    3333333     3:::::3");
-		getLogger().info("   / _ \\|  _ \\   3::::::33333::::::3 " + getDescription().getVersion().substring(1));
-		getLogger().info("  | |_| | | | |  3:::::::::::::::33");
-		getLogger().info("   \\___/|_| |_|   333333333333333");
-		getLogger().info("  ");
-		getLogger().info(" ...has enabled successfully!");
+		getLogger().info(" ...version " + getDescription().getVersion() + " has " + status + " successfully!");
 	}
 
 	private boolean checkForCensoredLib()
@@ -120,22 +101,17 @@ public class DemigodsPlugin extends CensoredJavaPlugin
 		Plugin check = Bukkit.getPluginManager().getPlugin("CensoredLib");
 		if(check instanceof CensoredLibPlugin && check.getDescription().getVersion().startsWith(CENSORED_LIBRARY_VERSION)) return true;
 		getLogger().severe("  ");
-		getLogger().severe("  ");
 		getLogger().severe("                  888        d8b   888");
-		getLogger().severe("                  888        Y8P   888");
 		getLogger().severe("                  888              888");
 		getLogger().severe("        .d8888b   888        888   88888b.");
 		getLogger().severe("       d88P\"      888        888   888 \"88b");
-		getLogger().severe("       888        888        888   888  888");
 		getLogger().severe("       Y88b.      888        888   888 d88P");
 		getLogger().severe("        \"Y8888P   88888888   888   88888P\"");
-		getLogger().severe("  ");
 		getLogger().severe("  ");
 		getLogger().severe(" CensoredLib was not found (or did not to load).");
 		getLogger().severe(" Demigods cannot load without CensoredLib enabled!");
 		getLogger().severe(" Please go to the BukkitDev project page to");
 		getLogger().severe(" download and install CensoredLib.");
-		getLogger().severe("  ");
 		getLogger().severe("  ");
 		getLogger().severe(" http://dev.bukkit.org/bukkit-plugins/censoredlib/");
 		getLogger().severe("  ");
@@ -146,7 +122,6 @@ public class DemigodsPlugin extends CensoredJavaPlugin
 		getLogger().severe("  ");
 		getLogger().severe("  ");
 		getLogger().severe(" ~~~~ _Alex & HmmmQuestionMark, CensoredSoftware");
-		getLogger().severe("  ");
 		getLogger().severe("  ");
 		return false;
 	}
