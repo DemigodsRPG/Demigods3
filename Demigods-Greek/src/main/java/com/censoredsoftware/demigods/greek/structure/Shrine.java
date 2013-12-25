add86b9027c3a1b4ff5c944cd93dfa837671c02b
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-25 18:33:46 -0500

    Message!

--- Demigods-Greek/src/main/java/com/censoredsoftware/demigods/greek/structure/Shrine.java
+++ Demigods-Greek/src/main/java/com/censoredsoftware/demigods/greek/structure/Shrine.java
@@ -65,7 +65,7 @@ public class Shrine
 		{
 			if(!DCharacter.Util.areAllied(character, DataManager.characters.get(data.getOwner()))) return false;
 			Location location = data.getReferenceLocation();
-			location.getWorld().playSound(location, Sound.CAT_PURR, 0.3F, 0.7F);
+			location.getWorld().playSound(location, Sound.CAT_PURR, 0.6F, 0.8F);
 			MaterialData colorData = Colors.getMaterial(character.getDeity().getColor());
 			location.getWorld().playEffect(location.clone().add(0, 1, 0), Effect.STEP_SOUND, colorData.getItemTypeId(), colorData.getData());
 			return true;
@@ -80,6 +80,7 @@ public class Shrine
 			Location location = data.getReferenceLocation();
 			location.getWorld().playSound(location, Sound.WITHER_HURT, 0.4F, 1.5F);
 			location.getWorld().playEffect(location.clone().add(0, 1, 0), Effect.STEP_SOUND, Material.REDSTONE_BLOCK.getId());
+			character.getOfflinePlayer().getPlayer().sendMessage(ChatColor.RED + "This shrine has " + (data.getSanctity() - data.getCorruption()) + " sanctity left!");
 			return true;
 		}
 	};
