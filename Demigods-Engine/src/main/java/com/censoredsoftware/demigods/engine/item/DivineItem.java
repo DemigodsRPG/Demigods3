14f8b9608afc5ba02f385c6bbfeabdaf589bdd51
Alex Bennett <me@alexben.net>
2013-12-29 01:34:13 -0500

    Implement Divine Item flags into the listeners.

--- Demigods-Engine/src/main/java/com/censoredsoftware/demigods/engine/item/DivineItem.java
+++ Demigods-Engine/src/main/java/com/censoredsoftware/demigods/engine/item/DivineItem.java
@@ -1,10 +1,16 @@
 package com.censoredsoftware.demigods.engine.item;
 
+import java.util.NoSuchElementException;
 import java.util.Set;
 
 import org.bukkit.event.Listener;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.Recipe;
+
+import com.censoredsoftware.censoredlib.util.Items;
+import com.censoredsoftware.demigods.engine.Demigods;
+import com.google.common.base.Predicate;
+import com.google.common.collect.Iterables;
 
 public interface DivineItem
 {
@@ -27,11 +33,39 @@ public interface DivineItem
 
 	public enum Flag
 	{
-		UNENCHANTABLE, UNREPAIRABLE;
+		UNENCHANTABLE;
 	}
 
 	public enum Category
 	{
 		ARMOR, WEAPON, CONSUMABLE, BOOK;
+	}
+
+	public static class Util
+	{
+		public static DivineItem findDivineItem(final ItemStack item)
+		{
+			try
+			{
+				return Iterables.find(Demigods.mythos().getDivineItems(), new Predicate<DivineItem>()
+				{
+					@Override
+					public boolean apply(DivineItem foundItem)
+					{
+						return Items.areEqualIgnoreEnchantments(foundItem.getItem(), item);
+					}
+				});
+			}
+			catch(NoSuchElementException ignored)
+			{}
+
+			return null;
+		}
+
+		public static boolean hasFlag(final ItemStack item, Flag flag)
+		{
+			DivineItem divineItem = findDivineItem(item);
+			return divineItem != null && divineItem.getFlags().contains(flag);
+		}
 	}
 }
