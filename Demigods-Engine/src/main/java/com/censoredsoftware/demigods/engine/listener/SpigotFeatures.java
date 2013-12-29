7dfb635cf612871a8b4940939995bacbe20f25cd
HmmmQuestionMark <ajchauncey@gmail.com>
2013-12-28 19:33:56 -0500

    Let's see if this works for #151.

---
+++ Demigods-Engine/src/main/java/com/censoredsoftware/demigods/engine/listener/SpigotFeatures.java
@@ -0,0 +1,51 @@
+package com.censoredsoftware.demigods.engine.listener;
+
+import com.censoredsoftware.censoredlib.trigger.Trigger;
+import com.censoredsoftware.censoredlib.util.Threads;
+import com.censoredsoftware.demigods.engine.Demigods;
+import com.censoredsoftware.demigods.engine.player.DCharacter;
+import com.censoredsoftware.demigods.engine.player.DPlayer;
+import com.censoredsoftware.demigods.engine.structure.Structure;
+import com.censoredsoftware.demigods.engine.structure.StructureData;
+import com.censoredsoftware.demigods.engine.util.Zones;
+import com.google.common.collect.Iterables;
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+
+public class SpigotFeatures implements Listener
+{
+	public SpigotFeatures()
+	{
+		Threads.registerTrigger(Demigods.PLUGIN, new Trigger()
+		{
+			@Override
+			public void processSync()
+			{
+				for(Player online : Bukkit.getOnlinePlayers())
+				{
+					Location location = online.getLocation();
+					DCharacter character = DPlayer.Util.getPlayer(online).getCurrent();
+					if(Zones.inNoDemigodsZone(location) || character == null || !Structure.Util.isInRadiusWithFlag(location, Structure.Flag.NO_GRIEFING))
+					{
+						online.spigot().setCollidesWithEntities(true);
+						continue;
+					}
+					StructureData save = Iterables.getFirst(Structure.Util.getInRadiusWithFlag(location, Structure.Flag.NO_GRIEFING), null);
+					if(save == null || save.getOwner().equals(character.getId()))
+					{
+						online.spigot().setCollidesWithEntities(true);
+						continue;
+					}
+					online.spigot().setCollidesWithEntities(false);
+				}
+
+			}
+
+			@Override
+			public void processAsync()
+			{}
+		});
+	}
+}
